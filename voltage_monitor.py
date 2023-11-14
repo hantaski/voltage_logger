7f3dbfc5238ddfadfd75530e3c6c0a485fee73d4
Hannu Taskinen <hannu.taskinen@gmail.com>
2023-11-14 13:13:47 +0200

    stuff

--- voltage_monitor.py
+++ voltage_monitor.py
@@ -9,43 +9,15 @@ import os
 import adafruit_sdcard
 import digitalio
 import storage
-import json
+import asyncio
+import adafruit_ds1307
+import wifi
+import adafruit_ntp
+import socketpool
+import gc
+import adafruit_requests as requests
 from analogio import AnalogIn
-
-
-def init_sd():
-    # Use any pin that is not taken by SPI
-    SD_CS = board.GP17
-
-    # Connect to the card and mount the filesystem.
-    spi = busio.SPI(board.GP18, board.GP19, board.GP16)
-    cs = digitalio.DigitalInOut(SD_CS)
-    sdcard = adafruit_sdcard.SDCard(spi, cs)
-    vfs = storage.VfsFat(sdcard)
-    storage.mount(vfs, "/sd")
-
-
-def init_display():
-    global splash
-    displayio.release_displays()
-    i2c = busio.I2C (scl=board.GP1, sda=board.GP0, frequency=100000) # This RPi Pico way to call I2C
-    display_bus = displayio.I2CDisplay (i2c, device_address = 0x3C) # The address of my Board
-    display = adafruit_displayio_sh1106.SH1106(display_bus, width=128, height=64)
-    splash = displayio.Group()
-    display.show(splash)
-    color_bitmap = displayio.Bitmap(128, 64, 1) # Full screen white
-    color_palette = displayio.Palette(1)
-    color_palette[0] = 0xFFFFFF  # White
-
-    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-    splash.append(bg_sprite)
-
-    # Draw a smaller inner rectangle
-    inner_bitmap = displayio.Bitmap(118, 54, 1)
-    inner_palette = displayio.Palette(1)
-    inner_palette[0] = 0x000000  # Black
-    inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=5, y=4)
-    splash.append(inner_sprite)
+import ssl
 
 def convert(value):
     OldMin = 208
@@ -54,25 +26,124 @@ def convert(value):
     NewMax = 15
     return (((value - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
 
+def pr(s):
+    text_area.text = s
 
-init_display()
-init_sd()
-analog_in = AnalogIn(board.GP26)
+def boo(s):
+    return str(s or '').lower() in ['true', '1', 't', 'y', 'yes']
 
-# Draw a label
-text = "Heippadeippa"
-text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=28, y=15)
+async def read_voltage():
+    while True:
+        # read voltage
+        global voltage
+        voltage = analog_in.value
+        ctime = rtc.datetime
+        td = "{hour:02d}:{minute:02d}\n{day}.{month}.{year}".format(hour=ctime.tm_hour, minute=ctime.tm_min, year=ctime.tm_year, month=ctime.tm_mon, day=ctime.tm_mday)
+        text = td+"\n{v:2.2f}".format(v=convert(voltage))+"V´"
+        pr(text)
+        await asyncio.sleep(int(os.getenv('display_frequency')))
+
+async def store_voltage():
+    while True:
+        ctime = rtc.datetime
+        td = "{year}.{month}.{day} - {hour:02d}:{minute:02d}:{second:02d}".format(hour=ctime.tm_hour, minute=ctime.tm_min, second=ctime.tm_sec, year=ctime.tm_year, month=ctime.tm_mon, day=ctime.tm_mday)
+        if (boo(os.getenv('use_sdcard'))):
+            print(convert(voltage))
+            with open("/sd/voltage_log.txt", "a+") as f:
+                f.write("{ti} - {volt:2.2f}\r\n".format(ti=td, volt=convert(voltage)))
+                f.flush()
+        if boo(os.getenv('use_wifi')) and boo(os.getenv('use_influxdb')) :
+            while not wifi.radio.connected:
+                await asyncio.sleep(1)
+
+            data = "voltage,location=office voltage={volt}".format(volt=convert(voltage))
+            try:
+                response = http_session.post(INFLUXDB_URL, headers=HEADERS, data=data)
+                if response.status_code == 204:
+                    print("Data sent to InfluxDB successfully!")
+                    pass
+                else:
+                    print("Failed to send data to InfluxDB:", response.text)
+                response.close()
+            except Exception as e:
+                print("Error sending data to InfluxDB:", e)
+        await asyncio.sleep(int(os.getenv('store_frequency')))
+
+async def wifi_connect():
+    while True:
+        if not wifi.radio.connected:
+            pr("Connecting to\n"+ssid)
+            try:
+                wifi.radio.connect(ssid, psk)
+                pr("WIFI connected!")
+            except Exception as e:
+                pr("Failed to connect:"+str(e))
+                print("Failed to connect:"+str(e))
+                await asyncio.sleep(10)
+        else:
+            await asyncio.sleep(20)
+
+
+async def main():
+    global pool,http_session
+    asyncio.create_task(wifi_connect())
+    while not wifi.radio.connected:
+        await asyncio.sleep(1)
+    await asyncio.sleep(2)
+    if (boo(os.getenv('use_wifi'))):
+        pool = socketpool.SocketPool(wifi.radio)
+        ssl_context = ssl.create_default_context()
+        http_session = requests.Session(pool, ssl_context)
+        if (boo(os.getenv('use_ntp'))):
+            ntp = adafruit_ntp.NTP(pool, tz_offset=int(os.getenv('timezone')))
+            current_time_struct = ntp.datetime
+            # store ntp to rtc
+            rtc.datetime = current_time_struct
+    asyncio.create_task(read_voltage())
+    asyncio.create_task(store_voltage())
+    while True:
+        gc.collect()
+        await asyncio.sleep(10)
+
+
+ssid = os.getenv('ssid')
+psk = os.getenv("psk")
+INFLUXDB_URL_BASE = os.getenv('influxdb_url')
+INFLUXDB_ORG = os.getenv('influxdb_org')
+INFLUXDB_BUCKET = os.getenv('influxdb_bucket')
+INFLUXDB_TOKEN = os.getenv('influxdb_token')
+
+# Construct the full InfluxDB URL with org and bucket parameters
+INFLUXDB_URL = f"{INFLUXDB_URL_BASE}?org={INFLUXDB_ORG}&bucket={INFLUXDB_BUCKET}"
+
+HEADERS = {
+    "Authorization": f"Token {INFLUXDB_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+displayio.release_displays()
+
+# init display&rtc
+i2c = busio.I2C(scl=board.GP1, sda=board.GP0, frequency=100000)
+rtc = adafruit_ds1307.DS1307(i2c)
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+display = adafruit_displayio_sh1106.SH1106(display_bus, rotation=180, width=128, height=64)
+splash = displayio.Group()
+display.root_group = splash
+text = "Starting.."
+text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=8, y=15)
 splash.append(text_area)
 
-#file = open("/sd/config.json")
-#file = open("/config.json")
-#data = json.load(file)
-poll_frequency = os.getenv('poll_frequency')
-print("poll_frequency="+str(poll_frequency))
+# init sdcard
+SD_CS = board.GP17
+spi = busio.SPI(board.GP18, board.GP19, board.GP16)
+cs = digitalio.DigitalInOut(SD_CS)
+sdcard = adafruit_sdcard.SDCard(spi, cs)
+if (boo(os.getenv('use_sdcard'))):
+    vfs = storage.VfsFat(sdcard)
+    storage.mount(vfs, "/sd")
 
+analog_in = AnalogIn(board.GP26)
+voltage = 0.0
 
-while True:
-    n = analog_in.value
-    print(str(n)+" = "+str(convert(n)))
-    time.sleep(poll_frequency)
-    pass
+asyncio.run(main())
